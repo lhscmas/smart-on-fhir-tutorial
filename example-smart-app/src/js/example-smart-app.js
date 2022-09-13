8b186a691ba3ac4ccbca9a89d92d8f7fc66fc9bb
lhscmas <113321181+lhscmas@users.noreply.github.com>
2022-09-13 13:55:13 -0400

    Create example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -64,6 +64,7 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.temperature = getQuantityValueAndUnit(temperature[0]);
 
           ret.resolve(p);
         });
