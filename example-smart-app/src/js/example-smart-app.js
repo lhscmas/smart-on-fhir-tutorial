ab5c73bc0228f93708af1ebfabbcadbc7a8dd694
lhscmas <113321181+lhscmas@users.noreply.github.com>
2022-09-13 15:18:29 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -76,12 +76,13 @@
           p.ldl = getQuantityValueAndUnit(ldl[0]);
           p.temperature = getQuantityValueAndUnit(temperature[0]);
           
+          p.allergies = ""
           allergies.forEach(function(allergy){
             if (typeof(allergy.code) === "object") {
-              console.log(allergy.code.text);
+              p.allergies +="<div>" + allergy.code.text + "</div>";
             }
           })
-          p.allergies = ""
+          
 
           ret.resolve(p);
         });
